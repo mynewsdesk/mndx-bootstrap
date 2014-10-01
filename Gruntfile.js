5952e44da5b24f1b585d66ccba94bc0232b0fa18
Nicolas MEDDA <nicolas.medda@mynewsdesk.com>
2014-10-01 11:40:25 +0200

    Update npm modules + grunt task for hologram

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,18 +4,29 @@ module.exports = function(grunt) {
       dist: {
         files: {
           'dist/mnd-bootstrap-dev.css': 'src/mnd-bootstrap-dev.scss'
+        },
+        options: {
+          loadPath: './bower_components'
         }
       }
     },
     watch: {
       css: {
         files: '**/*.scss',
-        tasks: ['sass']
+        tasks: ['sass', 'hologram']
+      }
+    },
+    hologram: {
+      build: {
+        options: {
+          config: 'hologram_config.yml'
+        }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['watch']);
-}
+  grunt.loadNpmTasks('grunt-hologram');
+  grunt.registerTask('default', ['sass', 'hologram', 'watch']);
+};
