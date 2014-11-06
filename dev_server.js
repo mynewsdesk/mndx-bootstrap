58d6ad64f6da0b44e45f3527ec516b2c586081dc
Nicolas MEDDA <b2l.powa@gmail.com>
2014-11-06 09:37:46 +0100

    Updates

--- dev_server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
-
-app.use(express.static(__dirname + '/public'));
-
-app.listen(port, function() {
-  console.log('Server listening on port ' + port);
-});
