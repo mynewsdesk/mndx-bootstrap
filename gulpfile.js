7d8c9f9c2b33f422e4c4ad8e6ff13177f672101b
Nicolas MEDDA <nicolas.medda@mynewsdesk.com>
2015-01-07 14:22:04 +0100

    Add `gulp sass` task
    Compile the `.scss` files to `dist/`

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
 var hologram = require('gulp-hologram');
 var notify = require('gulp-notify');
-
 
 // Dev task: build, serve and watch
 gulp.task('default', function() {
   gulp.start('hologram');
+});
+
+// Compile sass files
+gulp.task('sass', function() {
+  return gulp.src('src/mnd-bootstrap*.scss')
+    .pipe(sass({ style: 'expanded', loadPath: ['./bower_components/'] }))
+    .on('error', function (err) { console.log(err.message); })
+    .pipe(gulp.dest('dist/'))
+    .pipe(notify({message: 'Sass task complete'}));
 });
 
 // Compile style-guide
