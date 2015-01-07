04092a27fb8041e032479b82b3dab474f16b3a7c
Nicolas MEDDA <nicolas.medda@mynewsdesk.com>
2015-01-07 14:13:44 +0100

    Add `gulp hologram` task
    Compile the style-guide

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
 var gulp = require('gulp');
+var hologram = require('gulp-hologram');
+var notify = require('gulp-notify');
 
-gulp.task('default', function() {});
+
+// Dev task: build, serve and watch
+gulp.task('default', function() {
+  gulp.start('hologram');
+});
+
+// Compile style-guide
+gulp.task('hologram', ['sass'], function() {
+  return gulp.src('hologram_config.yml')
+    .pipe(hologram())
+    .pipe(notify({message: 'Hologram task complete'}));
+});
