f2ea2f3b2fdd02be2c116f7b2283345431075d30
Nicolas MEDDA <b2l.powa@gmail.com>
2015-05-21 16:31:27 +0200

    Rename the file to snake_case

--- hologram/mnd.js
+++ hologram/mnd.js
@@ -1,3 +1,3 @@
 window.React = require('react');
-window.DatePicker = require('components/datepicker');
+window.DatePicker = require('components/date_picker');
 window.Dropdown = require('components/dropdown');
