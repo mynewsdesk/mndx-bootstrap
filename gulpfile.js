1f2eafc10568ab4b1a99a5505779e201ecd096b0
Joakim Olander <joakim.westerlund@gmail.com>
2015-09-08 13:48:48 +0200

    Include browserSync and Node-sass
    Replace gulp-ruby-sass with gulp-sass for faster processing performance.
    Changed to use browserSync for live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
 var bump = require('gulp-bump');
 var deploy = require('gulp-gh-pages');
 var git = require('gulp-git');
@@ -7,6 +8,7 @@ var notify = require('gulp-notify');
 var runSequence = require('run-sequence');
 var request = require('superagent');
 var sass = require('gulp-ruby-sass');
+var gulpSass = require('gulp-sass');
 var webserver = require('gulp-webserver');
 var clean = require('gulp-rimraf');
 
@@ -14,9 +16,38 @@ var argv = require('yargs')
   .default('level', 'minor')
   .argv;
 
+var reload = browserSync.reload;
+
+var config = {
+    bowerDir: './bower_components',
+    bootstrapDir: './bower_components/bootstrap-sass',
+    publicDir: './public'
+};
+
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: config.publicDir
+    }
+  });
+  gulp.start('watch-scss');
+});
+
 // Dev task: build, serve and watch
 gulp.task('default', function() {
-  gulp.start('webserver', 'watch');
+  gulp.start('serve', 'watch-styleguide', 'mnd-bootstrap-sass');
+});
+
+gulp.task('documentation', function() {
+  gulp.start('styleguide', 'serve', 'watch-styleguide', 'sass')
+});
+
+// Serve the style-guide
+gulp.task('webserver', function() {
+  gulp.src('public')
+    .pipe(webserver({
+      livereload: true
+    }));
 });
 
 // Release a new version and update the doc (style-guide)
@@ -24,31 +55,38 @@ gulp.task('release', function(done) {
   runSequence('tag', 'rails-assets', 'gh-pages', done);
 });
 
-// Recompile the styleguide on scss file change
-gulp.task('watch', function() {
-  gulp.watch(['src/**/*.scss', 'doc_assets/**/*.scss' ,'doc_assets/**/*.html'], ['hologram']);
-  gulp.watch(['src/**/*.js'], ['hologram']);
-});
-
-// Compile sass files
-gulp.task('sass', function() {
-  return gulp.src('src/mnd-bootstrap.scss')
-    .pipe(sass({ style: 'compressed', loadPath: ['./bower_components/'] }))
-    .on('error', function (err) { console.log(err.message); })
-    .pipe(gulp.dest('dist/'))
-    .pipe(notify({message: 'Sass task complete'}));
-});
-
 gulp.task('clean', function() {
   return gulp.src('public/dist/').pipe(clean());
 });
+
+gulp.task('watch-scss', function (){
+  gulp.watch(['src/**/*.scss'], ['mnd-bootstrap-sass']);
+});
+
+// Recompile the styleguide on scss file change
+gulp.task('watch-styleguide', function() {
+  gulp.watch(['src/**/*.scss', 'doc_assets/**/*.scss' ,'doc_assets/**/*.html'], ['styleguide-sass','styleguide']);
+  gulp.watch(['src/**/*.js'], ['styleguide']);
+});
+
+gulp.task('mnd-bootstrap-sass', function() {
+  return gulp.src('src/mnd-bootstrap.scss')
+    .pipe(gulpSass({includePaths: [config.bowerDir]}))
+    .on('error', function (err) { console.log(err.message); })
+    .pipe(gulp.dest('dist/'))
+    .pipe(gulp.dest('public/dist'))
+    .pipe(notify({message: 'mnd-bootstrap-sass task complete'}))
+    .pipe(reload({ stream:true }));
+});
+
+
 
 gulp.task('styleguide-sass', function() {
   return gulp.src('doc_assets/styleguide.scss')
     .pipe(sass({ style: 'expanded', loadPath: ['src/', './bower_components/'] }))
     .on('error', function (err) { console.log(err.message); })
     .pipe(gulp.dest('public/'))
-    .pipe(notify({message: 'Sass task complete'}));
+    .pipe(notify({message: 'Styleguide-sass task complete'}));
 });
 
 gulp.task('styleguide-assets', function() {
@@ -63,19 +101,12 @@ gulp.task('styleguide-assets', function() {
 });
 
 // Compile style-guide
-gulp.task('hologram', ['clean', 'sass', 'styleguide-sass', 'styleguide-assets'], function() {
+gulp.task('styleguide', ['clean', 'mnd-bootstrap-sass', 'styleguide-sass', 'styleguide-assets'], function() {
   return gulp.src('hologram_config.yml')
     .pipe(hologram())
     .pipe(notify({message: 'Hologram task complete'}));
 });
 
-// Serve the style-guide
-gulp.task('webserver', ['hologram'], function() {
-  gulp.src('public')
-    .pipe(webserver({
-      livereload: true
-    }));
-});
 
 // Increment version number
 gulp.task('bump', function() {
@@ -116,7 +147,7 @@ gulp.task('rails-assets', function(done) {
 });
 
 // Push ./public to gh-pages after hologram build
-gulp.task('gh-pages', ['hologram'], function() {
+gulp.task('gh-pages', ['styleguide'], function() {
   return gulp.src('./public/**/*')
     .pipe(deploy());
 });
